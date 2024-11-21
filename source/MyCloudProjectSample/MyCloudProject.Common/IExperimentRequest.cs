ce7723519765e770a6e66a3e685118aa255bcb20
RahmanKM <parizat2@gmail.com>
2024-11-22 00:30:00 +0100

    configured experiment request message

--- source/MyCloudProjectSample/MyCloudProject.Common/IExperimentRequest.cs
+++ source/MyCloudProjectSample/MyCloudProject.Common/IExperimentRequest.cs
@@ -5,26 +5,69 @@ using System.Text;
 namespace MyCloudProject.Common
 {
     /// <summary>
-    /// Defines the contract for the message request that will run your experiment.
+    /// Defines the contract for the experiment request message.
+    /// This interface is used to structure the input data required to execute an experiment.
+    /// It includes details such as experiment ID, input file, descriptive metadata, and various parameter values.
     /// </summary>
     public interface IExperimentRequest
     {
         /// <summary>
-        /// Any identifier of yout choice.
+        /// Gets or sets the unique identifier for the experiment request.
+        /// This is used to distinguish between multiple experiment requests.
         /// </summary>
-        public string ExperimentId { get; set; }
+        string ExperimentId { get; set; }
 
         /// <summary>
-        /// The URI of the file that contains the input arguments for your experiment.
+        /// Gets or sets the name of the experiment.
+        /// Provides a human-readable identifier for the experiment.
         /// </summary>
-        public string InputFile { get; set; }
+        string Name { get; set; }
 
-        public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the description of the experiment.
+        /// Offers additional context about the purpose or configuration of the experiment.
+        /// </summary>
+        string Description { get; set; }
 
-        public string Description { get; set; }
+        /// <summary>
+        /// Gets or sets the first configurable value for the experiment 1.
+        /// Used for parameters that control specific aspects of the experiment.
+        /// </summary>
+        string Value1 { get; set; }
 
-        public string MessageId { get; set; }
+        /// <summary>
+        /// Gets or sets the second configurable value for the experiment 2.
+        /// Similar to Value1, this is another parameter for experiment configuration.
+        /// </summary>
+        string Value2 { get; set; }
 
-        public string MessageReceipt { get; set; }
+        /// <summary>
+        /// Gets or sets the third configurable value for the experiment 3.
+        /// Unlike Value1 and Value2, this is a double value used for numeric configurations.
+        /// </summary>
+        double Value3 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the scalar encoder parameter for AQI (Air Quality Index).
+        /// Used to preprocess or encode AQI data for input into the experiment model.
+        /// </summary>
+        string ScalarEncoderAQI { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time information for the data row being processed.
+        /// This is useful for time-series data or experiments dependent on temporal information.
+        /// </summary>
+        string DateTimeDataRow { get; set; }
+
+        /// <summary>
+        /// Gets or sets the unique identifier for the queue message associated with the experiment request.
+        /// </summary>
+        string MessageId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the pop receipt acknowledging the retrieval of the queue message.
+        /// </summary>
+        string PopReceipt { get; set; }
     }
 }
+
--- source/MyCloudProjectSample/MyExperiment/ExerimentRequestMessage.cs
+++ source/MyCloudProjectSample/MyExperiment/ExerimentRequestMessage.cs
@@ -7,11 +7,62 @@ namespace MyExperiment
 {
     internal class ExerimentRequestMessage : IExperimentRequest
     {
+        /// <summary>
+        /// Gets or sets the unique identifier for the experiment request.
+        /// This is used to distinguish between multiple experiment requests.
+        /// </summary>
         public string ExperimentId { get; set; }
-        public string InputFile { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the experiment.
+        /// Provides a human-readable identifier for the experiment.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description of the experiment.
+        /// Offers additional context about the purpose or configuration of the experiment.
+        /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first configurable value for the experiment 1.
+        /// Used for parameters that control specific aspects of the experiment.
+        /// </summary>
+        public string Value1 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second configurable value for the experiment 2.
+        /// Similar to Value1, this is another parameter for experiment configuration.
+        /// </summary>
+        public string Value2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the third configurable value for the experiment 3.
+        /// Unlike Value1 and Value2, this is a double value used for numeric configurations.
+        /// </summary>
+        public double Value3 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the scalar encoder parameter for AQI (Air Quality Index).
+        /// Used to preprocess or encode AQI data for input into the experiment model.
+        /// </summary>
+        public string ScalarEncoderAQI { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time information for the data row being processed.
+        /// This is useful for time-series data or experiments dependent on temporal information.
+        /// </summary>
+        public string DateTimeDataRow { get; set; }
+
+        /// <summary>
+        /// Gets or sets the unique identifier for the queue message associated with the experiment request.
+        /// </summary>
         public string MessageId { get; set; }
-        public string MessageReceipt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the pop receipt acknowledging the retrieval of the queue message.
+        /// </summary>
+        public string PopReceipt { get; set; }
     }
 }
